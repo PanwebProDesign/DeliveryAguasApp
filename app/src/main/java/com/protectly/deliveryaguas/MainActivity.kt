536d298a3d777dc748c1e244adb449f15079edfb
Rodrigo <fsecosroma@gmail.com>
2024-07-18 14:02:21 -0400

    Delivery Aguas

---
+++ app/src/main/java/com/protectly/deliveryaguas/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.protectly.deliveryaguas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.protectly.deliveryaguas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    //viewbinding
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
